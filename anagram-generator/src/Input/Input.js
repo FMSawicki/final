57329f07ad4ccd08ffce921c35ebfc0df73e4c2d
Felix Sawicki <fmsawicki@gmail.con>
2018-11-11 12:00:06 -0500

    Initial commit, set up generator, input, and output directories and files. made new logo

---
+++ anagram-generator/src/Input/Input.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Input.css";
+
+class Input extends Component {
+  render() {
+    return (
+      <div classname="inputMain">
+        <p classname="inputText">Input your text here</p>
+      </div>
+    );
+  }
+}
